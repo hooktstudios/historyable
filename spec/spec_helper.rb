65d642f7c3bf3b7b7782f4cbe1072891013a8d97
Philippe Dionne <dionne.phil@gmail.com>
2013-09-13 16:32:12 -0400

    Configure rspec to allow usage of 'focus'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,10 @@ RSpec.configure do |config|
 
   config.include DatabaseMacros
 
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
   config.before(:suite) do
     Database.setup
     Database.run_default_migration
