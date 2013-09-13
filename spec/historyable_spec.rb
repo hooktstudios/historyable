afc25e94b82ef11b18bf67a2b222a65fee9d259e
Philippe Dionne <dionne.phil@gmail.com>
2013-09-13 16:33:02 -0400

    Add specs

---
+++ spec/historyable_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+
+describe Historyable do
+
+  before do
+    run_migration do
+      create_table(:cats, force: true) do |t|
+        t.string :name
+        t.string :age
+      end
+
+      create_table(:dogs, force: true) do |t|
+        t.string :name
+        t.string :age
+      end
+    end
+  end
+
+  class Cat < ActiveRecord::Base
+    include Historyable
+
+    attr_accessor :age
+
+    has_history :name
+  end
+
+  class Dog < ActiveRecord::Base
+    include Historyable
+
+    attr_accessor :age
+
+    has_history :name
+  end
+
+  let(:cat) { Cat.create }
+  let(:dog) { Dog.create }
+
+  describe :InstanceMethods do
+    before do
+      cat.update_attribute(:name, 'Garfield')
+    end
+
+    describe :name_history? do
+      it { expect(cat.name_history?).to be_true }
+      it { expect(dog.name_history?).to be_false }
+    end
+
+    describe :name_history do
+      it { expect(cat.name_history).to be_an_instance_of(Array) }
+      it { expect(cat.name_history.first).to be_a_kind_of(Hash) }
+      it { expect(cat.name_history.first[:attribute_value]).to eq('Garfield') }
+      it { expect(dog.name_history).to be_an_instance_of(Array) }
+      it { expect(dog.name_history).to be_empty }
+
+    end
+
+    describe :name_history_raw do
+      it { expect(cat.name_history_raw).to be_a_kind_of(ActiveRecord::Relation) }
+      it { expect(cat.name_history_raw.first).to be_an_instance_of(Change) }
+      it { expect(cat.name_history_raw.first[:object_attribute_value]).to eq('Garfield') }
+    end
+  end
+
+  describe :Callbacks do
+
+    describe :save_changes do
+      it { expect{ cat.update_attribute(:name, 'Garfield') }.to change { cat.name_history.size }.from(0).to(1) }
+      it { expect{ cat.update_attribute(:age, 6) }.to_not change { cat.name_history.size } }
+    end
+  end
+end
